0816c748560c3d1e1f61591e4fe8b4eedd92bf58
Jasmine Iwanek <jasmine@iwanek.co.uk>
2016-05-02 15:05:20 -0700

    update versioning

--- src/main/java/modernmetals/ModernMetals.java
+++ src/main/java/modernmetals/ModernMetals.java
@@ -44,7 +44,7 @@ public class ModernMetals
     public static ModernMetals INSTANCE = null;
     public static final String MODID = "modernmetals";
     public static final String NAME = "Modern Metals";
-    public static final String VERSION = "0.10.0";
+    public static final String VERSION = "0.10.1";
     
 	/** All ore-spawn files discovered in the ore-spawn folder */
 	public static final List<Path> oreSpawnConfigFiles = new LinkedList<>();
