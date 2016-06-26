da65dda77664d29a1b887bf51807c0ea05fb5f0c
Jasmine Iwanek <jasmine@iwanek.co.uk>
2016-06-26 16:39:08 -0700

    Fix achievements

--- src/main/java/modernmetals/init/Achievements.java
+++ src/main/java/modernmetals/init/Achievements.java
@@ -13,9 +13,9 @@ public abstract class Achievements {
 	public static AchievementPage page;
 
 	public static Achievement aluminumbrass_maker; // make blend
-	public static Achievement galvanizedsteel_maker; // make blend
+	public static Achievement galvanized_steel_maker; // make blend
 	public static Achievement nichrome_maker; // make blend
-	public static Achievement stainlesssteel_maker; // make blend
+	public static Achievement stainless_steel_maker; // make blend
 	public static Achievement titanium_maker; // make blend
 
 	private static boolean initDone = false;
@@ -26,9 +26,9 @@ public abstract class Achievements {
 		AchievementPage.registerAchievementPage(page);
 
 		aluminumbrass_maker = makeAchievement("aluminumbrass_maker",cyano.basemetals.init.Achievements.metallurgy,0,0,Items.aluminumbrass_ingot);
-		galvanizedsteel_maker = makeAchievement("galvanizedsteel_maker",cyano.basemetals.init.Achievements.metallurgy,0,1,Items.galvanizedsteel_ingot);
+		galvanized_steel_maker = makeAchievement("galvanized_steel_maker",cyano.basemetals.init.Achievements.metallurgy,0,1,Items.galvanizedsteel_ingot);
 		nichrome_maker = makeAchievement("nichrome_maker",cyano.basemetals.init.Achievements.metallurgy,0,2,Items.nichrome_ingot);
-		stainlesssteel_maker = makeAchievement("stainnesssteel_maker",cyano.basemetals.init.Achievements.metallurgy,0,3,Items.stainlesssteel_ingot);
+		stainless_steel_maker = makeAchievement("stainless_steel_maker",cyano.basemetals.init.Achievements.metallurgy,0,3,Items.stainlesssteel_ingot);
 		titanium_maker = makeAchievement("titanium_maker",cyano.basemetals.init.Achievements.metallurgy,0,4,Items.titanium_ingot);
 
 		initDone = true;
