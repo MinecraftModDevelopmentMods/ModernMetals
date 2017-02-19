9620850a8fdcbae38b5e6e44092534fb4afa4c10
knoxhack <hackofgod@gmail.com>
2017-02-19 01:14:46 -0500

    Some balances!

--- src/main/java/com/mcmoddev/orespawn/ModernMetalsOreSpawn.java
+++ src/main/java/com/mcmoddev/orespawn/ModernMetalsOreSpawn.java
@@ -12,18 +12,18 @@ public class ModernMetalsOreSpawn implements Function<OreSpawnAPI, SpawnLogic> {
 		SpawnLogic logic = api.createSpawnLogic();
 
 		logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD)
-				.addOre(Materials.aluminum.ore.getDefaultState(), 8, 4, 8, 0,  96)
-				.addOre(Materials.cadmium.ore.getDefaultState(), 8, 4,  8, 0,  96)
-				.addOre(Materials.chromium.ore.getDefaultState(), 8, 4, 3, 0, 32)
-				.addOre(Materials.iridium.ore.getDefaultState(), 8, 4, 5, 0, 64)
-				.addOre(Materials.magnesium.ore.getDefaultState(), 8, 4, 8, 0, 96)
-				.addOre(Materials.manganese.ore.getDefaultState(), 8, 4, 6, 0, 64)
-				.addOre(Materials.osmium.ore.getDefaultState(), 8, 4, 6, 0, 96)
-				.addOre(Materials.plutonium.ore.getDefaultState(), 8, 4, 4, 0, 32)
-				.addOre(Materials.rutile.ore.getDefaultState(), 8, 4, 6, 0, 64)
-				.addOre(Materials.tantalum.ore.getDefaultState(), 8, 4, 6, 0, 64)
-				.addOre(Materials.tungsten.ore.getDefaultState(), 8, 4, 6, 0, 32)
-				.addOre(Materials.uranium.ore.getDefaultState(), 8, 4, 6, 0, 32)
+				.addOre(Materials.aluminum.ore.getDefaultState(), 10, 8, 8, 0,  96)
+				.addOre(Materials.cadmium.ore.getDefaultState(), 4, 8,  8, 0,  96)
+				.addOre(Materials.chromium.ore.getDefaultState(), 2, 4, 3, 0, 32)
+				.addOre(Materials.iridium.ore.getDefaultState(), 6, 4, 5, 0, 64)
+				.addOre(Materials.magnesium.ore.getDefaultState(), 6, 4, 8, 0, 96)
+				.addOre(Materials.manganese.ore.getDefaultState(), 6, 4, 6, 0, 64)
+				.addOre(Materials.osmium.ore.getDefaultState(), 10, 4, 8, 0, 96)
+				.addOre(Materials.plutonium.ore.getDefaultState(), 2, 4, 4, 0, 32)
+				.addOre(Materials.rutile.ore.getDefaultState(), 6, 4, 6, 0, 64)
+				.addOre(Materials.tantalum.ore.getDefaultState(), 6, 4, 6, 0, 64)
+				.addOre(Materials.tungsten.ore.getDefaultState(), 4, 4, 6, 0, 32)
+				.addOre(Materials.uranium.ore.getDefaultState(), 2, 4, 6, 0, 32)
 				.addOre(Materials.zirconium.ore.getDefaultState(), 8, 4, 6, 0, 64);
 
 		return logic;
