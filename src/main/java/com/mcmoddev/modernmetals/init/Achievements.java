46a8b783ede1afe65a19c60ce7d0f7824a5a324c
dshadowwolf <dshadowwolf@gmail.com>
2017-05-01 18:08:58 -0400

    API updates and buildability fixes (#55)
    * update to match changes in BMe/MMDLib for base-class of plugins name change
    
    * I'm a derp and forgot to guard against null materials in direct calls and that it would also effect chained calls
    
    * fix the same sort of error here that BMe had
    
    * Since the helpers in CoFHLib/CoFHCore were fixed and the temporary copies in the plugin base removed, this code needs updated to use the real helper functions
    
    * fix the last big bug from the TE api update
    
    * rename/refactor fixes for fallout from MMDLib embedded in BMe rename/refactor of 4/1
    
    * updates for BMe API change in MMDMaterial
    
    * BMe now requires the Sponge maven, so now so does everything that deps on BMe
    
    * update the CreativeTabs API
    
    * fix a thinko - make the API use actually correct
    
    * bugfixes, yay!
    
    * API/Build update - also fixes up the TE plugin so it shouldn't try passing in null ItemStack entries

--- src/main/java/com/mcmoddev/modernmetals/init/Achievements.java
+++ src/main/java/com/mcmoddev/modernmetals/init/Achievements.java
@@ -35,28 +35,28 @@ public class Achievements extends com.mcmoddev.lib.init.Achievements {
 			return;
 		}
 
-		if (com.mcmoddev.basemetals.util.Config.Options.enableAchievements) {
+		if (com.mcmoddev.basemetals.util.Config.Options.enableAchievements()) {
 			AchievementPage page = new AchievementPage(Loader.instance().activeModContainer().getModId());
 			AchievementPage.registerAchievementPage(page);
 
 			if (Options.enableAluminumBrass) {
-				aluminumBrassMaker = makeAchievement("aluminumbrass_maker", com.mcmoddev.basemetals.init.Achievements.metallurgy, 0, 0, Materials.getMaterialByName("aluminumbrass").getItem(Names.INGOT), page);
+				aluminumBrassMaker = makeAchievement("aluminumbrass_maker", com.mcmoddev.basemetals.init.Achievements.getAchievementByName("metallurgy"), 0, 0, Materials.getMaterialByName("aluminumbrass").getItem(Names.INGOT), page);
 			}
 
 			if (Options.enableGalvanizedSteel) {
-				galvanizedSteelMaker = makeAchievement("galvanized_steel_maker", com.mcmoddev.basemetals.init.Achievements.metallurgy, 0, 1, Materials.getMaterialByName("galvanizedsteel").getItem(Names.INGOT), page);
+				galvanizedSteelMaker = makeAchievement("galvanized_steel_maker", com.mcmoddev.basemetals.init.Achievements.getAchievementByName("metallurgy"), 0, 1, Materials.getMaterialByName("galvanizedsteel").getItem(Names.INGOT), page);
 			}
 
 			if (Options.enableNichrome) {
-				nichromeMaker = makeAchievement("nichrome_maker", com.mcmoddev.basemetals.init.Achievements.metallurgy, 0, 2, Materials.getMaterialByName("nichrome").getItem(Names.INGOT), page);
+				nichromeMaker = makeAchievement("nichrome_maker", com.mcmoddev.basemetals.init.Achievements.getAchievementByName("metallurgy"), 0, 2, Materials.getMaterialByName("nichrome").getItem(Names.INGOT), page);
 			}
 
 			if (Options.enableStainlessSteel) {
-				stainlessSteelMaker = makeAchievement("stainless_steel_maker", com.mcmoddev.basemetals.init.Achievements.metallurgy, 0, 3, Materials.getMaterialByName("stainlesssteel").getItem(Names.INGOT), page);
+				stainlessSteelMaker = makeAchievement("stainless_steel_maker", com.mcmoddev.basemetals.init.Achievements.getAchievementByName("metallurgy"), 0, 3, Materials.getMaterialByName("stainlesssteel").getItem(Names.INGOT), page);
 			}
 
 			if (Options.enableTitanium) {
-				titaniumMaker = makeAchievement("titanium_maker", com.mcmoddev.basemetals.init.Achievements.metallurgy, 0, 4, Materials.getMaterialByName("titanium").getItem(Names.INGOT), page);
+				titaniumMaker = makeAchievement("titanium_maker", com.mcmoddev.basemetals.init.Achievements.getAchievementByName("metallurgy"), 0, 4, Materials.getMaterialByName("titanium").getItem(Names.INGOT), page);
 			}
 		}
 
