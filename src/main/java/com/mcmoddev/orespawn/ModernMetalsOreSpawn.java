b022100c1204bb99e2cf38c5a6c76348469884b4
dshadowwolf <dshadowwolf@gmail.com>
2017-07-07 02:38:57 -0400

    port to OreSpawn 3.1.x interfaces (#62)
    * clean up some SonarQube issues - hope you like the fix for the unused parameters bit, Jas!
    
    * Fix stuff up, remove the old OS1 interfacing code and update the OreSpawn plugin to the API of v3
    
    * Update ModernMetals OreSpawn integration so it works with OreSpawn 3.1.x

--- src/main/java/com/mcmoddev/orespawn/ModernMetalsOreSpawn.java
+++ src/main/java/com/mcmoddev/orespawn/ModernMetalsOreSpawn.java
@@ -1,71 +1,16 @@
 package com.mcmoddev.orespawn;
 
-import com.google.common.base.Function;
-import com.mcmoddev.lib.data.Names;
-import com.mcmoddev.modernmetals.data.MaterialNames;
-import com.mcmoddev.modernmetals.init.Materials;
-import com.mcmoddev.orespawn.api.OreSpawnAPI;
-import com.mcmoddev.orespawn.api.SpawnLogic;
-import com.mcmoddev.modernmetals.util.Config.Options;
+import com.mcmoddev.orespawn.api.os3.*;
+import com.mcmoddev.orespawn.api.plugin.IOreSpawnPlugin;
+import com.mcmoddev.orespawn.api.plugin.OreSpawnPlugin;
 
-public class ModernMetalsOreSpawn implements Function<OreSpawnAPI, SpawnLogic> {
-
+@OreSpawnPlugin( modid = "modernmetals", resourcePath = "orespawn" )
+public class ModernMetalsOreSpawn implements IOreSpawnPlugin {
+	
 	@Override
-	public SpawnLogic apply(OreSpawnAPI api) {
-		SpawnLogic logic = api.createSpawnLogic();
-
-		if (Options.materialEnabled(MaterialNames.ALUMINUM)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.ALUMINUM).getBlock(Names.ORE).getDefaultState(), 10, 8, 8, 0,  96);
-		}
-
-		if (Options.materialEnabled(MaterialNames.CADMIUM)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.CADMIUM).getBlock(Names.ORE).getDefaultState(), 4, 8,  8, 0,  96);
-		}
-
-		if (Options.materialEnabled(MaterialNames.CHROMIUM)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.CHROMIUM).getBlock(Names.ORE).getDefaultState(), 2, 4, 3, 0, 32);
-		}
-
-		if (Options.materialEnabled(MaterialNames.IRIDIUM)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.IRIDIUM).getBlock(Names.ORE).getDefaultState(), 6, 4, 5, 0, 64);
-		}
-
-		if (Options.materialEnabled(MaterialNames.MAGNESIUM)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.MAGNESIUM).getBlock(Names.ORE).getDefaultState(), 6, 4, 8, 0, 96);
-		}
-
-		if (Options.materialEnabled(MaterialNames.MANGANESE)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.MANGANESE).getBlock(Names.ORE).getDefaultState(), 6, 4, 6, 0, 64);
-		}
-
-		if (Options.materialEnabled(MaterialNames.OSMIUM)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.OSMIUM).getBlock(Names.ORE).getDefaultState(), 10, 4, 8, 0, 96);
-		}
-
-		if (Options.materialEnabled(MaterialNames.PLUTONIUM)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.PLUTONIUM).getBlock(Names.ORE).getDefaultState(), 2, 4, 4, 0, 32);
-		}
-
-		if (Options.materialEnabled(MaterialNames.RUTILE)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.RUTILE).getBlock(Names.ORE).getDefaultState(), 6, 4, 6, 0, 64);
-		}
-
-		if (Options.materialEnabled(MaterialNames.TANTALUM)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.TANTALUM).getBlock(Names.ORE).getDefaultState(), 6, 4, 6, 0, 64);
-		}
-
-		if (Options.materialEnabled(MaterialNames.TUNGSTEN)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.TUNGSTEN).getBlock(Names.ORE).getDefaultState(), 4, 4, 6, 0, 32);
-		}
-
-		if (Options.materialEnabled(MaterialNames.URANIUM)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.URANIUM).getBlock(Names.ORE).getDefaultState(), 2, 4, 6, 0, 32);
-		}
-
-		if (Options.materialEnabled(MaterialNames.ZIRCONIUM)) {
-			logic.getDimension(OreSpawnAPI.DIMENSION_WILDCARD).addOre(Materials.getMaterialByName(MaterialNames.ZIRCONIUM).getBlock(Names.ORE).getDefaultState(), 8, 4, 6, 0, 64);
-		}
-
-		return logic;
+	public void register(OS3API apiInterface) {
+		// nothing for us to do - all of our ores are in the
+		// jar and the code handles that
 	}
+	
 }
