8c9bb7149bfa7fcdad5c3efc01b04632ca1dfc5a
Daniel R. Hazelton <dshadowwolf@gmail.com>
2019-01-21 16:06:31 -0500

    update version in prep for push to CF

--- src/main/java/com/mcmoddev/modernmetals/ModernMetals.java
+++ src/main/java/com/mcmoddev/modernmetals/ModernMetals.java
@@ -49,7 +49,7 @@ public class ModernMetals {
 	 * increased whenever a change is made that has the potential to break
 	 * compatibility with other mods that depend on this one.
 	 */
-	protected static final String VERSION = "2.5.0-beta6";
+	protected static final String VERSION = "2.5.0-beta7";
 
 	protected static final String UPDATEJSON = SharedStrings.UPDATE_JSON_URL + "ModernMetals/master/update.json";
 
